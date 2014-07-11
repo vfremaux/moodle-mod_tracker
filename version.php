9eafe852dde3c2f937a8b56d5188d1e468176dc9
valery <valery.fremaux@gmail.com>
2014-07-11 18:44:52 +0200

    add events and recode some tables renderings

--- version.php
+++ version.php
@@ -1,9 +1,9 @@
 <?php
 
-$module->version  = 2014010400;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2014071100;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2013111800;
 $module->component = 'mod_tracker';   // Full name of the plugin (used for diagnostics)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 $module->maturity = MATURITY_RC;
-$module->release = '2.6.0 (Build 2014010400)';
+$module->release = '2.7.0 (Build 2014071100)';
 
