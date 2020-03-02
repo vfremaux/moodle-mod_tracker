220965ddb7641ea1dfcb6583d357ffafd9605b4d
valery <valery.fremaux@gmail.com>
2020-03-02 14:25:56 +0100

    Automated sync from SVN

--- db/mnet.php
+++ db/mnet.php
@@ -23,6 +23,7 @@
  * @copyright 2012 Valery Fremaux
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 
 $publishes = array(
     'tracker' => array(
